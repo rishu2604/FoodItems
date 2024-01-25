0b03e15783e0a312212bfdcc811a33e229bf5a41
rishu2604 <82023279+rishu2604@users.noreply.github.com>
2024-01-25 14:10:06 +0530

    Update FoodList.jsx

--- src/components/FoodList.jsx
+++ src/components/FoodList.jsx
@@ -4,12 +4,17 @@ export default function FoodList({item}) {
     return (
         <div className='container pt-4'>
             <h3 className='pb-3'>Food Items</h3>
-            <ul className='list-group'>
-                {item.map((itemset, index)=>{
-                    return (<li key={index}className='list-group-item'>{itemset}</li>)
-                })}
-            </ul>
+            {item.length===0 && <div className="alert alert-danger" role="alert">
+                                    No food available!
+                                </div>}
 
+            {item.length>0 &&
+                <ul className='list-group'>
+                    {item.map((itemset, index)=>{
+                        return (<li key={index}className='list-group-item rounded-pill bg-primary-subtle'>{itemset}</li>)
+                    })}
+                </ul>
+            }
         </div>
     )
 }
